e4245193e2ae7eb30054b4528bd70e7566c0fa04
Yann Gautier <yann.gautier@st.com>
2019-02-15 16:45:48 +0100

    drivers: tzc400: add support for interrupts
    Change-Id: Ie9ab1c199a8f12b2c9472d7120efbdf35711284a
    Signed-off-by: Yann Gautier <yann.gautier@st.com>

--- drivers/arm/tzc/tzc400.c
+++ drivers/arm/tzc/tzc400.c
@@ -10,6 +10,7 @@
 #include <common/debug.h>
 #include <drivers/arm/tzc400.h>
 #include <lib/mmio.h>
+#include <lib/utils_def.h>
 
 #include "tzc_common_private.h"
 
@@ -60,7 +61,6 @@ static inline void _tzc400_write_gate_keeper(uintptr_t base, unsigned int val)
 					GATE_KEEPER_OS_SHIFT) &		\
 					GATE_KEEPER_OS_MASK)
 
-
 /* Define common core functions used across different TZC peripherals. */
 DEFINE_TZC_COMMON_WRITE_ACTION(400, 400)
 DEFINE_TZC_COMMON_WRITE_REGION_BASE(400, 400)
@@ -70,8 +70,44 @@ DEFINE_TZC_COMMON_WRITE_REGION_ID_ACCESS(400, 400)
 DEFINE_TZC_COMMON_CONFIGURE_REGION0(400)
 DEFINE_TZC_COMMON_CONFIGURE_REGION(400)
 
+static inline void tzc400_clear_it(long base, uint32_t filter)
+{
+	mmio_write_32(base + INT_CLEAR, BIT(filter));
+}
+
+static inline uint32_t tzc400_get_int_by_filter(long base, uint32_t filter)
+{
+	return (mmio_read_32(base + INT_STATUS) & BIT(filter));
+}
+
+#if DEBUG
+static unsigned long tzc400_get_fail_address(long base, uint32_t filter)
+{
+	unsigned long fail_address;
+
+	fail_address = mmio_read_32(base + FAIL_ADDRESS_LOW_OFF +
+				    (filter * FILTER_OFFSET));
+#ifdef __aarch64__
+	fail_address += mmio_read_32(base + FAIL_ADDRESS_HIGH_OFF +
+				     (filter * FILTER_OFFSET)) << 32;
+#endif
+
+	return fail_address;
+}
+
+static inline uint32_t tzc400_get_fail_id(long base, uint32_t filter)
+{
+	return mmio_read_32(base + FAIL_ID + (filter * FILTER_OFFSET));
+}
+
+static inline uint32_t tzc400_get_fail_control(long base, uint32_t filter)
+{
+	return mmio_read_32(base + FAIL_CONTROL_OFF + (filter * FILTER_OFFSET));
+}
+#endif
+
 static unsigned int _tzc400_get_gate_keeper(uintptr_t base,
-				unsigned int filter)
+					    unsigned int filter)
 {
 	unsigned int open_status;
 
@@ -81,9 +117,8 @@ static unsigned int _tzc400_get_gate_keeper(uintptr_t base,
 }
 
 /* This function is not MP safe. */
-static void _tzc400_set_gate_keeper(uintptr_t base,
-				unsigned int filter,
-				int val)
+static void _tzc400_set_gate_keeper(uintptr_t base, unsigned int filter,
+				    int val)
 {
 	unsigned int open_status;
 
@@ -151,7 +186,7 @@ void tzc400_init(uintptr_t base)
  * changed. This function only changes the access permissions.
  */
 void tzc400_configure_region0(unsigned int sec_attr,
-			   unsigned int ns_device_access)
+			      unsigned int ns_device_access)
 {
 	assert(tzc400.base != 0U);
 	assert(sec_attr <= TZC_REGION_S_RDWR);
@@ -168,11 +203,11 @@ void tzc400_configure_region0(unsigned int sec_attr,
  * for this region (see comment for that function).
  */
 void tzc400_configure_region(unsigned int filters,
-			  unsigned int region,
-			  unsigned long long region_base,
-			  unsigned long long region_top,
-			  unsigned int sec_attr,
-			  unsigned int nsaid_permissions)
+			     unsigned int region,
+			     unsigned long long region_base,
+			     unsigned long long region_top,
+			     unsigned int sec_attr,
+			     unsigned int nsaid_permissions)
 {
 	assert(tzc400.base != 0U);
 
@@ -185,7 +220,7 @@ void tzc400_configure_region(unsigned int filters,
 	 * the max and expected case.
 	 */
 	assert((region_top <= (UINT64_MAX >> (64U - tzc400.addr_width))) &&
-		(region_base < region_top));
+	       (region_base < region_top));
 
 	/* region_base and (region_top + 1) must be 4KB aligned */
 	assert(((region_base | (region_top + 1U)) & (4096U - 1U)) == 0U);
@@ -193,8 +228,7 @@ void tzc400_configure_region(unsigned int filters,
 	assert(sec_attr <= TZC_REGION_S_RDWR);
 
 	_tzc400_configure_region(tzc400.base, filters, region, region_base,
-						region_top,
-						sec_attr, nsaid_permissions);
+				 region_top, sec_attr, nsaid_permissions);
 }
 
 void tzc400_enable_filters(void)
@@ -217,8 +251,8 @@ void tzc400_enable_filters(void)
 			 * See the 'ARM (R) CoreLink TM TZC-400 TrustZone (R)
 			 * Address Space Controller' Technical Reference Manual.
 			 */
-			ERROR("TZC-400 : Filter %d Gatekeeper already"
-				" enabled.\n", filter);
+			ERROR("TZC-400: Filter %d Gatekeeper already enabled\n",
+			      filter);
 			panic();
 		}
 		_tzc400_set_gate_keeper(tzc400.base, filter, 1);
@@ -237,4 +271,63 @@ void tzc400_disable_filters(void)
 	 */
 	for (filter = 0; filter < tzc400.num_filters; filter++)
 		_tzc400_set_gate_keeper(tzc400.base, filter, 0);
+}
+
+void tzc400_it_handler(void)
+{
+	uint32_t filter;
+	uint32_t filter_it_pending = tzc400.num_filters;
+#if DEBUG
+	uint32_t control_fail;
+	uint32_t fail_id;
+	unsigned long address_fail;
+#endif
+
+	assert(tzc400.base != 0U);
+
+	/* first display information conerning the fault access */
+	for (filter = 0U; (filter < tzc400.num_filters) &&
+	     (filter_it_pending == tzc400.num_filters); filter++) {
+		if (tzc400_get_int_by_filter(tzc400.base, filter)) {
+			filter_it_pending = filter;
+		}
+	}
+
+	if (filter_it_pending == tzc400.num_filters) {
+		ERROR("Error no IT pending!");
+		panic();
+	}
+
+#if DEBUG
+	address_fail = tzc400_get_fail_address(tzc400.base, filter_it_pending);
+	ERROR("Illegal access to 0x%lx:\n", address_fail);
+
+	fail_id = tzc400_get_fail_id(tzc400.base, filter_it_pending);
+	ERROR("\tFAIL_ID = 0x%x\n", fail_id);
+
+	control_fail = tzc400_get_fail_control(tzc400.base, filter_it_pending);
+
+	if (((control_fail & BIT(FAIL_CONTROL_NS_SHIFT)) >> FAIL_CONTROL_NS_SHIFT) ==
+	    FAIL_CONTROL_NS_NONSECURE) {
+		ERROR("\tNon-Secure\n");
+	} else {
+		ERROR("\tSecure\n");
+	}
+
+	if (((control_fail & BIT(FAIL_CONTROL_PRIV_SHIFT)) >> FAIL_CONTROL_PRIV_SHIFT) ==
+	    FAIL_CONTROL_PRIV_PRIV) {
+		ERROR("\tPrivilege\n");
+	} else {
+		ERROR("\tUnprivilege\n");
+	}
+
+	if (((control_fail & BIT(FAIL_CONTROL_DIR_SHIFT)) >> FAIL_CONTROL_DIR_SHIFT) ==
+	    FAIL_CONTROL_DIR_WRITE) {
+		ERROR("\tWrite\n");
+	} else {
+		ERROR("\tRead\n");
+	}
+#endif
+
+	tzc400_clear_it(tzc400.base, filter_it_pending);
 }
--- include/drivers/arm/tzc400.h
+++ include/drivers/arm/tzc400.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2018, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2014-2020, ARM Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -32,7 +32,6 @@
 #define BUILD_CONFIG_AW_MASK			U(0x3f)
 #define BUILD_CONFIG_NR_SHIFT			0
 #define BUILD_CONFIG_NR_MASK			U(0x1f)
-
 /*
  * Number of gate keepers is implementation defined. But we know the max for
  * this device is 4. Get implementation details from BUILD_CONFIG.
@@ -93,6 +92,8 @@
 #define TZC_400_REGION_SIZE			U(0x20)
 #define TZC_400_ACTION_OFF			U(0x4)
 
+#define FILTER_OFFSET				U(0x10)
+
 #ifndef __ASSEMBLER__
 
 #include <cdefs.h>
@@ -103,46 +104,44 @@
  ******************************************************************************/
 void tzc400_init(uintptr_t base);
 void tzc400_configure_region0(unsigned int sec_attr,
-			   unsigned int ns_device_access);
+			      unsigned int ns_device_access);
 void tzc400_configure_region(unsigned int filters,
-			  unsigned int region,
-			  unsigned long long region_base,
-			  unsigned long long region_top,
-			  unsigned int sec_attr,
-			  unsigned int nsaid_permissions);
+			     unsigned int region,
+			     unsigned long long region_base,
+			     unsigned long long region_top,
+			     unsigned int sec_attr,
+			     unsigned int nsaid_permissions);
 void tzc400_set_action(unsigned int action);
 void tzc400_enable_filters(void);
 void tzc400_disable_filters(void);
+void tzc400_it_handler(void);
 
 static inline void tzc_init(uintptr_t base)
 {
 	tzc400_init(base);
 }
 
-static inline void tzc_configure_region0(
-			unsigned int sec_attr,
-			unsigned int ns_device_access)
+static inline void tzc_configure_region0(unsigned int sec_attr,
+					 unsigned int ns_device_access)
 {
 	tzc400_configure_region0(sec_attr, ns_device_access);
 }
 
-static inline void tzc_configure_region(
-			  unsigned int filters,
-			  unsigned int region,
-			  unsigned long long region_base,
-			  unsigned long long region_top,
-			  unsigned int sec_attr,
-			  unsigned int ns_device_access)
+static inline void tzc_configure_region(unsigned int filters,
+					unsigned int region,
+					unsigned long long region_base,
+					unsigned long long region_top,
+					unsigned int sec_attr,
+					unsigned int ns_device_access)
 {
 	tzc400_configure_region(filters, region, region_base,
-			region_top, sec_attr, ns_device_access);
+				region_top, sec_attr, ns_device_access);
 }
 
 static inline void tzc_set_action(unsigned int action)
 {
 	tzc400_set_action(action);
 }
-
 
 static inline void tzc_enable_filters(void)
 {
