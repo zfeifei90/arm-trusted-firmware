b288f8792d104031afcc84fc74672935aac93d91
Yann Gautier <yann.gautier@st.com>
2019-05-21 09:12:08 +0200

    stm32mp1: add TAMPER support
    Change-Id: I91e0257387a509e0188e5299c196192039d9e54d
    Signed-off-by: Yann Gautier <yann.gautier@st.com>

--- plat/st/stm32mp1/sp_min/sp_min-stm32mp1.mk
+++ plat/st/stm32mp1/sp_min/sp_min-stm32mp1.mk
@@ -12,6 +12,7 @@ BL32_SOURCES		+=	plat/common/aarch32/platform_mp_stack.S		\
 				drivers/st/etzpc/etzpc.c			\
 				drivers/st/rng/stm32_rng.c			\
 				drivers/st/rtc/stm32_rtc.c			\
+				drivers/st/tamper/stm32_tamp.c			\
 				plat/st/stm32mp1/sp_min/sp_min_setup.c		\
 				plat/st/stm32mp1/stm32mp1_low_power.c		\
 				plat/st/stm32mp1/stm32mp1_pm.c			\
