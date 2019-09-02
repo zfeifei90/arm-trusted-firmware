921d42ab3ff030f9cfa14772d1b2c9ded2e10447
Lionel Debieve <lionel.debieve@st.com>
2019-09-02 18:15:45 +0200

    clk: stm32mp1: keep RTCAPB clock always enabled
    Further information such as boot instance are sent over backup
    registers. In order to guarantee direct access to backup registers
    in uboot, we will keep the RTC clock enabled.
    
    Change-Id: I16572d422bfebbf39190a87db8046df486ce91c8
    Signed-off-by: Lionel Debieve <lionel.debieve@st.com>
    Signed-off-by: Yann Gautier <yann.gautier@st.com>

--- drivers/st/clk/stm32mp1_clk.c
+++ drivers/st/clk/stm32mp1_clk.c
@@ -2344,6 +2344,7 @@ static void sync_earlyboot_clocks_state(void)
 		DDRC2, DDRC2LP,
 		DDRCAPB, DDRPHYCAPB, DDRPHYCAPBLP,
 		DDRPHYC, DDRPHYCLP,
+		RTCAPB,
 		TZC1, TZC2,
 		TZPC,
 		STGEN_K,
@@ -2351,10 +2352,6 @@ static void sync_earlyboot_clocks_state(void)
 
 	for (idx = 0U; idx < ARRAY_SIZE(secure_enable); idx++) {
 		stm32mp_clk_enable(secure_enable[idx]);
-	}
-
-	if (!stm32mp_is_single_core()) {
-		stm32mp1_clk_enable_secure(RTCAPB);
 	}
 }
 
