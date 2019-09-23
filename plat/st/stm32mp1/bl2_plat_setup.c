fb95583ecfb81ff2611760f7b4ba9b390dfa4b20
Yann Gautier <yann.gautier@st.com>
2019-09-23 17:25:46 +0200

    stm32mp1: initialize PMIC earlier
    In order to have PMIC regulators available for storage drivers,
    PMIC should be initialized before IO drivers initialization.
    
    Change-Id: Ife18ef9510ee13ae090209eca4292496f230783e
    Signed-off-by: Yann Gautier <yann.gautier@st.com>

--- plat/st/stm32mp1/bl2_plat_setup.c
+++ plat/st/stm32mp1/bl2_plat_setup.c
@@ -142,10 +142,6 @@ void bl2_platform_setup(void)
 	int ret;
 	uint32_t ddr_ns_size;
 
-	if (dt_pmic_status() > 0) {
-		initialize_pmic();
-	}
-
 	ret = stm32mp1_ddr_probe();
 	if (ret < 0) {
 		ERROR("Invalid DDR init: error %d\n", ret);
@@ -397,6 +393,10 @@ skip_console_init:
 	print_reset_reason();
 
 	update_monotonic_counter();
+
+	if (dt_pmic_status() > 0) {
+		initialize_pmic();
+	}
 
 	stm32mp_io_setup();
 }
