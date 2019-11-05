477a7419522102bd6c8b4c90379df0c62abfeb5f
Nicolas Le Bayon <nicolas.le.bayon@st.com>
2019-11-05 18:31:41 +0100

    clk: stm32mp1: clock tree init optimization
    Store each PLL node validity information,
    this avoids parsing DT several times.
    
    Change-Id: I17f4dae23fb44ac60bd0ad9ce989ce1c680ab505
    Signed-off-by: Nicolas Le Bayon <nicolas.le.bayon@st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/tf-a/+/149293
    Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
    Reviewed-by: Yann GAUTIER <yann.gautier@st.com>

--- drivers/st/clk/stm32mp1_clk.c
+++ drivers/st/clk/stm32mp1_clk.c
@@ -1811,6 +1811,7 @@ int stm32mp1_clk_init(void)
 	int ret, len;
 	enum stm32mp1_pll_id i;
 	bool pllcsg_set[_PLL_NB];
+	bool pllcfg_valid[_PLL_NB];
 	bool lse_css = false;
 	bool pll3_preserve = false;
 	bool pll4_preserve = false;
@@ -1842,7 +1843,8 @@ int stm32mp1_clk_init(void)
 		snprintf(name, sizeof(name), "st,pll@%d", i);
 		plloff[i] = fdt_rcc_subnode_offset(name);
 
-		if (!fdt_check_node(plloff[i])) {
+		pllcfg_valid[i] = fdt_check_node(plloff[i]);
+		if (!pllcfg_valid[i]) {
 			continue;
 		}
 
@@ -2006,7 +2008,7 @@ int stm32mp1_clk_init(void)
 			continue;
 		}
 
-		if (!fdt_check_node(plloff[i])) {
+		if (!pllcfg_valid[i]) {
 			continue;
 		}
 
@@ -2029,7 +2031,7 @@ int stm32mp1_clk_init(void)
 	}
 	/* Wait and start PLLs ouptut when ready */
 	for (i = (enum stm32mp1_pll_id)0; i < _PLL_NB; i++) {
-		if (!fdt_check_node(plloff[i])) {
+		if (!pllcfg_valid[i]) {
 			continue;
 		}
 
