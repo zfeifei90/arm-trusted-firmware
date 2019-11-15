9f019655e9fe029717faaae11860911b51006b86
Nicolas Le Bayon <nicolas.le.bayon@st.com>
2019-11-15 15:56:06 +0100

    pmic: stm32mp: improve driver usage
    Store status of dt_pmic_status() as local static variable,
    this avoids parsing DT several times.
    In the same way, store nodes in dt_pmic_i2c_config() and
    in dt_get_pmic_node() as local static variables.
    
    Change-Id: I4585e9dfdde2847a369bffcc6f2b39ecc2b74de1
    Signed-off-by: Nicolas Le Bayon <nicolas.le.bayon@st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/tf-a/+/149283
    Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
    Reviewed-by: Lionel DEBIEVE <lionel.debieve@st.com>
    Reviewed-by: Etienne CARRIERE <etienne.carriere@st.com>
    Reviewed-by: Yann GAUTIER <yann.gautier@st.com>

--- drivers/st/pmic/stm32mp_pmic.c
+++ drivers/st/pmic/stm32mp_pmic.c
@@ -36,19 +36,34 @@ static uint32_t pmic_i2c_addr;
 
 static int dt_get_pmic_node(void)
 {
-	return dt_get_node_by_compatible("st,stpmic1");
+	static int node = -FDT_ERR_BADOFFSET;
+
+	if (node == -FDT_ERR_BADOFFSET) {
+		node = dt_get_node_by_compatible("st,stpmic1");
+	}
+
+	return node;
 }
 
 int dt_pmic_status(void)
 {
+	static int status = -FDT_ERR_BADVALUE;
 	int node;
+
+	if (status != -FDT_ERR_BADVALUE) {
+		return status;
+	}
 
 	node = dt_get_pmic_node();
 	if (node <= 0) {
-		return -FDT_ERR_NOTFOUND;
+		status = -FDT_ERR_NOTFOUND;
+
+		return status;
 	}
 
-	return fdt_get_status(node);
+	status = (int)fdt_get_status(node);
+
+	return status;
 }
 
 static bool dt_pmic_is_secure(void)
@@ -263,32 +278,35 @@ static int pmic_operate(uint8_t command, const char *node_name,
 static int dt_pmic_i2c_config(struct dt_node_info *i2c_info,
 			      struct stm32_i2c_init_s *init)
 {
-	int pmic_node, i2c_node;
+	static int i2c_node = -FDT_ERR_NOTFOUND;
+	int pmic_node;
 	void *fdt;
 	const fdt32_t *cuint;
 
-	if (fdt_get_address(&fdt) == 0) {
-		return -FDT_ERR_NOTFOUND;
-	}
+	if (i2c_node == -FDT_ERR_NOTFOUND) {
+		if (fdt_get_address(&fdt) == 0) {
+			return -FDT_ERR_NOTFOUND;
+		}
 
-	pmic_node = dt_get_pmic_node();
-	if (pmic_node < 0) {
-		return 1;
-	}
+		pmic_node = dt_get_pmic_node();
+		if (pmic_node < 0) {
+			return 1;
+		}
 
-	cuint = fdt_getprop(fdt, pmic_node, "reg", NULL);
-	if (cuint == NULL) {
-		return -FDT_ERR_NOTFOUND;
-	}
+		cuint = fdt_getprop(fdt, pmic_node, "reg", NULL);
+		if (cuint == NULL) {
+			return -FDT_ERR_NOTFOUND;
+		}
 
-	pmic_i2c_addr = fdt32_to_cpu(*cuint) << 1;
-	if (pmic_i2c_addr > UINT16_MAX) {
-		return -FDT_ERR_BADVALUE;
-	}
+		pmic_i2c_addr = fdt32_to_cpu(*cuint) << 1;
+		if (pmic_i2c_addr > UINT16_MAX) {
+			return -FDT_ERR_BADVALUE;
+		}
 
-	i2c_node = fdt_parent_offset(fdt, pmic_node);
-	if (i2c_node < 0) {
-		return -FDT_ERR_NOTFOUND;
+		i2c_node = fdt_parent_offset(fdt, pmic_node);
+		if (i2c_node < 0) {
+			return -FDT_ERR_NOTFOUND;
+		}
 	}
 
 	dt_fill_device_info(i2c_info, i2c_node);
