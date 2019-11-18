d62451f04228ca94f5b9825185d7d25abd8d999f
Nicolas Le Bayon <nicolas.le.bayon@st.com>
2019-11-18 15:46:59 +0100

    clk: stm32mp1: compute and manage pll1 settings
    Create a static structure containing, for each CPU operating point, the
    corresponding VDDCORE voltage and all PLL1 settings.
    All OPP PLL1 settings are computed during cold boot and saved in RAM.
    stm32mp1_clk_lp_save/load_opp_pll1_settings() functions allow to manage
    these settings in the context during the standby mode transitions.
    
    clk_compute_pll1_settings() uses a simplified but optimized algorithm,
    to compute PLL1 configuration and fractional values from the PLL12
    input frequency, given by the Device Tree, and the expected CPU frequency.
    clk_get_pll1_settings() just gets the input frequency before calling
    the algorithm. If they have already been computed, structure is parsed.
    
    A stm32mp1_clk_compute_all_pll1_settings() service gets frequency and
    voltage information from OPP table, and fills PLL1 settings structure,
    either by retrieving parameters in RCC registers (case of the current
    values, managed by clk_save_current_pll1_settings() function), or by
    computing them.
    
    stm32mp1_clk_init() calls clk_get_pll1_settings() when a non-null CPU
    expected frequency value is entered as new entry parameter, and when PLL1
    settings are not hardly-defined in the Device Tree.
    
    stm32mp1_clk_get_maxfreq_opp() parse OPP table in RAM to get OPP
    coordinates (frequency value and VDDCORE voltage) corresponding to the
    highest frequency couple.
    
    Change-Id: I4dd84c56a1d834858621ec1ba9c1f35cfa0a59de
    Signed-off-by: Gabriel Fernandez <gabriel.fernandez@st.com>
    Signed-off-by: Nicolas Le Bayon <nicolas.le.bayon@st.com>

--- include/drivers/st/stm32mp1_clk.h
+++ include/drivers/st/stm32mp1_clk.h
@@ -22,8 +22,16 @@ enum stm32mp_osc_id {
 
 extern const char *stm32mp_osc_node_label[NB_OSC];
 
+#define PLL1_SETTINGS_VALID_ID	U(0x504C4C31) /* "PLL1" */
+
 int stm32mp1_clk_probe(void);
-int stm32mp1_clk_init(void);
+int stm32mp1_clk_init(uint32_t pll1_freq_mhz);
+
+int stm32mp1_clk_compute_all_pll1_settings(uint32_t buck1_voltage);
+void stm32mp1_clk_lp_save_opp_pll1_settings(uint8_t *data, size_t size);
+void stm32mp1_clk_lp_load_opp_pll1_settings(uint8_t *data, size_t size);
+
+int stm32mp1_clk_get_maxfreq_opp(uint32_t *freq_mhz, uint32_t *voltage_mv);
 
 bool stm32mp1_rcc_is_secure(void);
 bool stm32mp1_rcc_is_mckprot(void);
