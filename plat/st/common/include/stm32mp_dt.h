4af1eafcd406a2c87ebc37553f8c17b9f681300e
Nicolas Le Bayon <nicolas.le.bayon@st.com>
2019-11-18 17:49:02 +0100

    stm32mp: get OPP parameters from DT
    Two new functions parse OPP table in device tree to get OPP parameters
    (frequency value and VDDCORE voltage).
    Compliance is checked with platform OPP HW support capabilities.
    
    dt_get_all_opp_freqvolt() returns all couples of values.
    dt_get_max_opp_freqvolt() returns only the highest frequency couple.
    
    Change-Id: I9b743eac548de635fb0eed45c50271856b5df771
    Signed-off-by: Nicolas Le Bayon <nicolas.le.bayon@st.com>

--- plat/st/common/include/stm32mp_dt.h
+++ plat/st/common/include/stm32mp_dt.h
@@ -39,6 +39,9 @@ int dt_get_node(struct dt_node_info *info, int offset, const char *compat);
 int dt_get_stdout_uart_info(struct dt_node_info *info);
 int dt_match_instance_by_compatible(const char *compatible, uintptr_t address);
 uint32_t dt_get_ddr_size(void);
+int dt_get_max_opp_freqvolt(uint32_t *freq_khz, uint32_t *voltage_mv);
+int dt_get_all_opp_freqvolt(uint32_t *count, uint32_t *freq_khz_array,
+			    uint32_t *voltage_mv_array);
 uint32_t dt_get_pwr_vdd_voltage(void);
 const char *dt_get_board_model(void);
 int fdt_get_gpio_bank_pin_count(unsigned int bank);
