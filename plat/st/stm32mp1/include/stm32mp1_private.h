74fca1adb859cb91f942803203b85932688139d5
Yann Gautier <yann.gautier@st.com>
2019-11-19 13:54:26 +0100

    stm32mp1: move get_uart_address() prototype to common header file
    This function should be defined for all ST platforms.
    
    Change-Id: I9b636d3b5e19d31c9b960f5349e1f252cc74d764
    Signed-off-by: Yann Gautier <yann.gautier@st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/tf-a/+/150064
    Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>

--- plat/st/stm32mp1/include/stm32mp1_private.h
+++ plat/st/stm32mp1/include/stm32mp1_private.h
@@ -23,9 +23,6 @@ void __dead2 stm32mp_wait_cpu_reset(void);
 
 void stm32mp1_arch_security_setup(void);
 void stm32mp1_security_setup(void);
-#if STM32MP_UART_PROGRAMMER
-uintptr_t get_uart_address(uint32_t instance_nb);
-#endif
 
 enum boot_device_e get_boot_device(void);
 
