dd7444ee6ecb8f408f3ee5279fab942a3e201467
Nicolas Le Bayon <nicolas.le.bayon@st.com>
2019-12-11 14:18:12 +0100

    ddr: stm32mp1_ddr: fix self refresh disable during DQS training
    DDRCTRL_PWRCTL.SELFREF_EN needs to be reset before DQS training step, not
    to enter in self refresh mode during the execution of this phase.
    Depending on settings, it can be set after the DQS training.
    
    Change-Id: I8aeede4854bd879fe25bcff75c99e0103a975753
    Signed-off-by: Nicolas Le Bayon <nicolas.le.bayon@st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/tf-a/+/152783
    Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
    Reviewed-by: CIBUILD <smet-aci-builds@lists.codex.cro.st.com>
    Reviewed-by: Etienne CARRIERE <etienne.carriere@st.com>
    Reviewed-by: Yann GAUTIER <yann.gautier@st.com>

--- drivers/st/ddr/stm32mp1_ddr.c
+++ drivers/st/ddr/stm32mp1_ddr.c
@@ -645,7 +645,8 @@ static void stm32mp1_refresh_disable(struct stm32mp1_ddrctl *ctl)
 	/* Quasi-dynamic register update*/
 	mmio_setbits_32((uintptr_t)&ctl->rfshctl3,
 			DDRCTRL_RFSHCTL3_DIS_AUTO_REFRESH);
-	mmio_clrbits_32((uintptr_t)&ctl->pwrctl, DDRCTRL_PWRCTL_POWERDOWN_EN);
+	mmio_clrbits_32((uintptr_t)&ctl->pwrctl, DDRCTRL_PWRCTL_POWERDOWN_EN |
+						 DDRCTRL_PWRCTL_SELFREF_EN);
 	mmio_clrbits_32((uintptr_t)&ctl->dfimisc,
 			DDRCTRL_DFIMISC_DFI_INIT_COMPLETE_EN);
 	stm32mp1_wait_sw_done_ack(ctl);
@@ -662,6 +663,10 @@ static void stm32mp1_refresh_restore(struct stm32mp1_ddrctl *ctl,
 	if ((pwrctl & DDRCTRL_PWRCTL_POWERDOWN_EN) != 0U) {
 		mmio_setbits_32((uintptr_t)&ctl->pwrctl,
 				DDRCTRL_PWRCTL_POWERDOWN_EN);
+	}
+	if ((pwrctl & DDRCTRL_PWRCTL_SELFREF_EN) != 0U) {
+		mmio_setbits_32((uintptr_t)&ctl->pwrctl,
+				DDRCTRL_PWRCTL_SELFREF_EN);
 	}
 	mmio_setbits_32((uintptr_t)&ctl->dfimisc,
 			DDRCTRL_DFIMISC_DFI_INIT_COMPLETE_EN);
