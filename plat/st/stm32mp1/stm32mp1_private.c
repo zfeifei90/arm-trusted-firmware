9a884b4e8f63790b758c94b334882a5f4949127b
Nicolas Le Bayon <nicolas.le.bayon@st.com>
2020-02-12 14:45:34 +0100

    stm32mp1: Start Auto/SW Self Refresh for LpDDR3
    As LpDDR2, enable automatic and software self refresh feature for LpDDR3.
    
    Change-Id: I93732cee4974cc7863b1aa2c9711cc52df180859
    Signed-off-by: Nicolas Le Bayon <nicolas.le.bayon@st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/tf-a/+/157859
    Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
    Reviewed-by: CIBUILD <smet-aci-builds@lists.codex.cro.st.com>
    Reviewed-by: Yann GAUTIER <yann.gautier@st.com>

--- plat/st/stm32mp1/stm32mp1_private.c
+++ plat/st/stm32mp1/stm32mp1_private.c
@@ -590,7 +590,8 @@ bool stm32mp_ddr_supports_ssr_asr(void)
 	uintptr_t ddrctrl_base = stm32mp_ddrctrl_base();
 	uint32_t mstr = mmio_read_32(ddrctrl_base + DDRCTRL_MSTR);
 
-	return (mstr & DDRCTRL_MSTR_LPDDR2) != 0U;
+	return ((mstr & DDRCTRL_MSTR_LPDDR2) != 0U) ||
+	       ((mstr & DDRCTRL_MSTR_LPDDR3) != 0U);
 }
 
 /* Return true when device is in closed state */
