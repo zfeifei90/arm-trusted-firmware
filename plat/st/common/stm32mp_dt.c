dc26ea64ce3139dd5d8e83aa60cb11574b346d0e
Yann Gautier <yann.gautier@st.com>
2020-03-11 17:17:51 +0100

    plat/st: correct static analysis tool warning
    Correct the following sparse warnings:
    plat/st/common/stm32mp_dt.c:103:5: warning:
     symbol 'fdt_get_node_parent_address_cells' was not declared.
     Should it be static?
    plat/st/common/stm32mp_dt.c:123:5: warning:
     symbol 'fdt_get_node_parent_size_cells' was not declared.
     Should it be static?
    
    Change-Id: Iad721f12128df83a3de3f53e7920a9c1dce64c56
    Signed-off-by: Yann Gautier <yann.gautier@st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/tf-a/+/161124
    Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
    Reviewed-by: CIBUILD <smet-aci-builds@lists.codex.cro.st.com>
    Reviewed-by: Nicolas LE BAYON <nicolas.le.bayon@st.com>

--- plat/st/common/stm32mp_dt.c
+++ plat/st/common/stm32mp_dt.c
@@ -90,6 +90,7 @@ uint8_t fdt_get_status(int node)
 	return status;
 }
 
+#if ENABLE_ASSERTIONS
 /*******************************************************************************
  * This function returns the address cells from the node parent.
  * Returns:
@@ -98,7 +99,7 @@ uint8_t fdt_get_status(int node)
  * - a default value if undefined #address-cells property as per libfdt
  *   implementation.
  ******************************************************************************/
-int fdt_get_node_parent_address_cells(int node)
+static int fdt_get_node_parent_address_cells(int node)
 {
 	int parent;
 
@@ -118,7 +119,7 @@ int fdt_get_node_parent_address_cells(int node)
  * - a default value if undefined #size-cells property as per libfdt
  *   implementation.
  ******************************************************************************/
-int fdt_get_node_parent_size_cells(int node)
+static int fdt_get_node_parent_size_cells(int node)
 {
 	int parent;
 
@@ -129,6 +130,7 @@ int fdt_get_node_parent_size_cells(int node)
 
 	return fdt_size_cells(fdt, parent);
 }
+#endif
 
 /*******************************************************************************
  * This function return interrupts from node.
