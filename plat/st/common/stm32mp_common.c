7f1b1d8582ef317f67b1603214b844a7df0920c8
Nicolas Le Bayon <nicolas.le.bayon@st.com>
2020-03-11 12:30:46 +0100

    stm32mp1: get CPU supply name
    Replace hard-coded regulator names by stm32mp_get_cpu_supply_name()
    platform service which gets CPU regulator name from DT and finds its
    related supply name.
    
    Change-Id: I8a0ed4cbb9c4df729ecda95220a24b9c2f429d0f
    Signed-off-by: Nicolas Le Bayon <nicolas.le.bayon@st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/tf-a/+/160675
    Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
    Reviewed-by: CIBUILD <smet-aci-builds@lists.codex.cro.st.com>
    Reviewed-by: Yann GAUTIER <yann.gautier@st.com>

--- plat/st/common/stm32mp_common.c
+++ plat/st/common/stm32mp_common.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2019, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2015-2020, ARM Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -12,6 +12,7 @@
 #include <arch_helpers.h>
 #include <common/debug.h>
 #include <drivers/st/stm32mp_clkfunc.h>
+#include <drivers/st/stm32mp_pmic.h>
 #include <lib/spinlock.h>
 #include <plat/common/platform.h>
 
@@ -144,4 +145,24 @@ int stm32mp_check_header(boot_api_image_header_t *header, uintptr_t buffer)
 	}
 
 	return 0;
+}
+
+/* Return CPU supply name */
+const char *stm32mp_get_cpu_supply_name(void)
+{
+	const char *regulator;
+	const char *supply = NULL;
+
+	regulator = dt_get_cpu_regulator_name();
+	if (regulator == NULL) {
+		return NULL;
+	}
+
+	if (dt_pmic_status() > 0) {
+		if (dt_pmic_find_supply(&supply, regulator) != 0) {
+			return NULL;
+		}
+	}
+
+	return supply;
 }
