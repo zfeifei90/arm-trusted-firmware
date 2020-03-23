4792b691530dde5c4b007e8f2a8fddc0420be2eb
Yann Gautier <yann.gautier@st.com>
2020-03-23 17:44:30 +0100

    pmic: stpmic1: update error cases return
    Use errno values, or the return of called functions, instead of -1.
    
    Change-Id: If7b53de5cbfb4d2c9979bce0e594dd92bf07a77a
    Signed-off-by: Yann Gautier <yann.gautier@st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/tf-a/+/161571
    Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
    Reviewed-by: CIBUILD <smet-aci-builds@lists.codex.cro.st.com>
    Reviewed-by: Etienne CARRIERE <etienne.carriere@st.com>
    Reviewed-by: Lionel DEBIEVE <lionel.debieve@st.com>

--- drivers/st/pmic/stpmic1.c
+++ drivers/st/pmic/stpmic1.c
@@ -1,9 +1,10 @@
 /*
- * Copyright (c) 2016-2019, STMicroelectronics - All Rights Reserved
+ * Copyright (c) 2016-2020, STMicroelectronics - All Rights Reserved
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+#include <errno.h>
 #include <string.h>
 
 #include <common/debug.h>
@@ -736,6 +737,7 @@ int stpmic1_regulator_voltage_get(const char *name)
 	const struct regul_struct *regul = get_regulator_data(name);
 	uint8_t value;
 	uint8_t mask;
+	int status;
 
 	/* Voltage can be set for buck<N> or ldo<N> (except ldo4) regulators */
 	if (strncmp(name, "buck", 4) == 0) {
@@ -747,14 +749,15 @@ int stpmic1_regulator_voltage_get(const char *name)
 		return 0;
 	}
 
-	if (stpmic1_register_read(regul->control_reg, &value)) {
-		return -1;
+	status = stpmic1_register_read(regul->control_reg, &value);
+	if (status < 0) {
+		return status;
 	}
 
 	value = (value & mask) >> LDO_BUCK_VOLTAGE_SHIFT;
 
 	if (value > regul->voltage_table_size) {
-		return -1;
+		return -ERANGE;
 	}
 
 	return (int)regul->voltage_table[value];
@@ -791,7 +794,7 @@ int stpmic1_register_write(uint8_t register_id, uint8_t value)
 		}
 
 		if (readval != value) {
-			return -1;
+			return -EIO;
 		}
 	}
 #endif
@@ -837,9 +840,11 @@ void stpmic1_dump_regulators(void)
 int stpmic1_get_version(unsigned long *version)
 {
 	uint8_t read_val;
+	int status;
 
-	if (stpmic1_register_read(VERSION_STATUS_REG, &read_val) != 0) {
-		return -1;
+	status = stpmic1_register_read(VERSION_STATUS_REG, &read_val);
+	if (status < 0) {
+		return status;
 	}
 
 	*version = (unsigned long)read_val;
