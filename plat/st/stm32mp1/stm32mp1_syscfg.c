3e96929125aba3c605792ffa3f4e78d7af88c407
Yann Gautier <yann.gautier@st.com>
2020-04-21 15:03:59 +0200

    stm32mp1: get CPU info from SYSCFG on STM32MP13
    The IDC register from DBGMCU is duplicated in SYSCFG. As SYSCFG is always
    accessible, get chip ID and revision ID from there on STM32MP13.
    
    Change-Id: Ib0b6e8f68a2934a45ec0012f69db6c12a60adb17
    Signed-off-by: Yann Gautier <yann.gautier@st.com>

--- plat/st/stm32mp1/stm32mp1_syscfg.c
+++ plat/st/stm32mp1/stm32mp1_syscfg.c
@@ -11,6 +11,7 @@
 #include <drivers/st/stm32mp1_clk.h>
 #include <drivers/st/stpmic1.h>
 #include <lib/mmio.h>
+#include <lib/utils_def.h>
 
 #include <stm32mp_common.h>
 #include <stm32mp_dt.h>
@@ -25,6 +26,7 @@
 #define SYSCFG_CMPCR				0x20U
 #define SYSCFG_CMPENSETR			0x24U
 #define SYSCFG_CMPENCLRR			0x28U
+#define SYSCFG_IDC				0x380U
 
 #define CMPCR_CMPENSETR_OFFSET			0x4U
 #define CMPCR_CMPENCLRR_OFFSET			0x8U
@@ -65,6 +67,13 @@
  * SYSCFG_CMPENSETR Register
  */
 #define SYSCFG_CMPENSETR_MPU_EN			BIT(0)
+
+/*
+ * SYSCFG_IDC Register
+ */
+#define SYSCFG_IDC_DEV_ID_MASK	GENMASK(11, 0)
+#define SYSCFG_IDC_REV_ID_MASK	GENMASK(31, 16)
+#define SYSCFG_IDC_REV_ID_SHIFT	16
 
 static void enable_io_comp_cell_finish(uintptr_t cmpcr_off)
 {
@@ -216,4 +225,23 @@ void stm32mp1_syscfg_disable_io_compensation(void)
 	disable_io_comp_cell(SYSCFG_CMPCR);
 
 	stm32mp1_clk_force_disable(SYSCFG);
+}
+
+/*
+ * @brief  Get silicon revision from SYSCFG registers.
+ * @retval chip version (REV_ID).
+ */
+uint32_t stm32mp1_syscfg_get_chip_version(void)
+{
+	return (mmio_read_32(SYSCFG_BASE + SYSCFG_IDC) &
+		SYSCFG_IDC_REV_ID_MASK) >> SYSCFG_IDC_REV_ID_SHIFT;
+}
+
+/*
+ * @brief  Get device ID from SYSCFG registers.
+ * @retval device ID (DEV_ID).
+ */
+uint32_t stm32mp1_syscfg_get_chip_dev_id(void)
+{
+	return mmio_read_32(SYSCFG_BASE + SYSCFG_IDC) & SYSCFG_IDC_DEV_ID_MASK;
 }
