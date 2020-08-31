96acdd0be9b7db6f027f30ca15d0ed5ce91c10d2
Pascal Paillet <p.paillet@st.com>
2020-08-31 11:17:30 +0200

    psci: export psci_is_last_on_cpu function
    psci_is_last_on_cpu() function becomes public.
    
    Signed-off-by: Pascal Paillet <p.paillet@st.com>
    Change-Id: I393219d0ea01207c08289e91d96f0a33888de10f
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/tf-a/+/176501
    Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
    Reviewed-by: CIBUILD <smet-aci-builds@lists.codex.cro.st.com>
    Reviewed-by: Lionel DEBIEVE <lionel.debieve@st.com>
    Reviewed-by: Yann GAUTIER <yann.gautier@st.com>

--- lib/psci/psci_private.h
+++ lib/psci/psci_private.h
@@ -285,7 +285,6 @@ unsigned int psci_find_max_off_lvl(const psci_power_state_t *state_info);
 unsigned int psci_find_target_suspend_lvl(const psci_power_state_t *state_info);
 void psci_set_pwr_domains_to_run(unsigned int end_pwrlvl);
 void psci_print_power_domain_map(void);
-unsigned int psci_is_last_on_cpu(void);
 int psci_spd_migrate_info(u_register_t *mpidr);
 void psci_do_pwrdown_sequence(unsigned int power_level);
 
