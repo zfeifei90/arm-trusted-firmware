bd0ce556ab552c2a4e84a7cbe78a6ffa964098dd
Patrick Delaunay <patrick.delaunay@st.com>
2020-09-14 09:55:33 +0200

    stm32mp1: add USB driver compilation
    Add DWC2 driver stack compilation for STM32MP_USB_PROGRAMMER target
    
    Signed-off-by: Patrick Delaunay <patrick.delaunay@st.com>
    Change-Id: Ic22dc232ba15c4aa555e4dff7dcb9a633f54db54
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/tf-a/+/177369
    Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
    Reviewed-by: Yann GAUTIER <yann.gautier@st.com>
    Tested-by: Yann GAUTIER <yann.gautier@st.com>

--- plat/st/stm32mp1/platform.mk
+++ plat/st/stm32mp1/platform.mk
@@ -189,7 +189,8 @@ BL2_SOURCES		+=	plat/st/stm32mp1/stm32mp1_boot_device.c
 endif
 
 ifeq (${STM32MP_USB_PROGRAMMER},1)
-BL2_SOURCES		+=	lib/usb/usb_core.c					\
+BL2_SOURCES		+=	drivers/st/usb_dwc2/usb_dwc2.c				\
+				lib/usb/usb_core.c					\
 				lib/usb/usb_st_dfu.c
 endif
 
