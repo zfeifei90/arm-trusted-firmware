6c879a5f56236160e7c09f3c8f91f933b199af2e
Lionel Debieve <lionel.debieve@st.com>
2020-10-06 11:38:45 +0200

    stm32mp: trusted boot support with FIP
    STM32 board with a FIP image can have same trusted boot as STM32 image.
    
    Signed-off-by: Lionel Debieve <lionel.debieve@st.com>
    Signed-off-by: Nicolas Toromanoff <nicolas.toromanoff@st.com>
    Change-Id: I2a7c4e4335af17cac7b74b0d6f1c65a74c60b22e
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/tf-a/+/184839
    Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
    Reviewed-by: CIBUILD <smet-aci-builds@lists.codex.cro.st.com>
    Reviewed-by: Yann GAUTIER <yann.gautier@st.com>

--- plat/st/common/stm32mp_trusted_boot.c
+++ plat/st/common/stm32mp_trusted_boot.c
@@ -11,10 +11,19 @@
 #include <platform_def.h>
 
 #include <common/debug.h>
+#include <common/tbbr/cot_def.h>
+#include <lib/fconf/fconf.h>
+#include <lib/fconf/fconf_dyn_cfg_getter.h>
+#include <lib/fconf/fconf_tbbr_getter.h>
 #include <plat/common/platform.h>
 
-static uint32_t root_pk_hash[BOOT_API_SHA256_DIGEST_SIZE_IN_BYTES /
-			     sizeof(uint32_t)];
+#if STM32MP_USE_STM32IMAGE
+static uint8_t root_pk_hash[BOOT_API_SHA256_DIGEST_SIZE_IN_BYTES];
+#else
+static uint8_t der_sha256_header[] = {0x30, 0x31, 0x30, 0x0d, 0x06, 0x09, 0x60,
+	0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x01, 0x05, 0x00, 0x04, 0x20};
+static uint8_t root_pk_hash[HASH_DER_LEN];
+#endif
 
 int plat_get_rotpk_info(void *cookie, void **key_ptr, unsigned int *key_len,
 			unsigned int *flags)
@@ -23,6 +32,7 @@ int plat_get_rotpk_info(void *cookie, void **key_ptr, unsigned int *key_len,
 	uint32_t otp_val;
 	uint32_t len;
 	size_t i;
+	size_t start_copy_idx = 0U;
 
 	if (cookie != NULL) {
 		return -EINVAL;
@@ -37,16 +47,28 @@ int plat_get_rotpk_info(void *cookie, void **key_ptr, unsigned int *key_len,
 		return -EINVAL;
 	}
 
-	for (i = 0U; i < ARRAY_SIZE(root_pk_hash); i++) {
+#if !STM32MP_USE_STM32IMAGE
+	memcpy(root_pk_hash, der_sha256_header, sizeof(der_sha256_header));
+	start_copy_idx = sizeof(der_sha256_header);
+#endif
+
+	for (i = 0U; i < BOOT_API_SHA256_DIGEST_SIZE_IN_BYTES / sizeof(uint32_t); i++) {
+		uint32_t temp;
+
 		if (stm32_get_otp_value_from_idx(otp_idx + i, &otp_val) != 0) {
 			return -EINVAL;
 		}
 
-		root_pk_hash[i] = bswap32(otp_val);
+		temp = bswap32(otp_val);
+		memcpy(root_pk_hash + i * sizeof(uint32_t) + start_copy_idx, &temp, sizeof(temp));
 	}
 
-	*key_ptr = &root_pk_hash;
+#if STM32MP_USE_STM32IMAGE
 	*key_len = BOOT_API_SHA256_DIGEST_SIZE_IN_BYTES;
+#else
+	*key_len = HASH_DER_LEN;
+#endif
+	*key_ptr = &root_pk_hash;
 	*flags = ROTPK_IS_HASH;
 
 	if (!stm32mp_is_closed_device()) {
@@ -80,12 +102,17 @@ int plat_get_rotpk_info(void *cookie, void **key_ptr, unsigned int *key_len,
 
 int plat_get_nv_ctr(void *cookie, unsigned int *nv_ctr)
 {
+#if STM32MP_USE_STM32IMAGE
+	if (cookie != NULL) {
+		return -EINVAL;
+	}
+#endif
+
 	/*
 	 * This monotonic counter is the counter used by ROM code
 	 * to identify BL2.
 	 */
-	if ((cookie == NULL) &&
-	    (stm32_get_otp_value(MONOTONIC_OTP, nv_ctr) == 0)) {
+	if (stm32_get_otp_value(MONOTONIC_OTP, nv_ctr) == 0) {
 		return 0;
 	}
 
@@ -100,6 +127,17 @@ int plat_set_nv_ctr(void *cookie, unsigned int nv_ctr)
 #if !STM32MP_USE_STM32IMAGE
 int plat_get_mbedtls_heap(void **heap_addr, size_t *heap_size)
 {
+	assert(heap_addr != NULL);
+	assert(heap_size != NULL);
+
+#if STM32MP_USE_EXTERNAL_HEAP
+	/* Retrieve the already allocated heap's info from DTB */
+	*heap_addr = FCONF_GET_PROPERTY(tbbr, dyn_config, mbedtls_heap_addr);
+	*heap_size = FCONF_GET_PROPERTY(tbbr, dyn_config, mbedtls_heap_size);
+
+	return 0;
+#else
 	return get_mbedtls_heap_helper(heap_addr, heap_size);
+#endif
 }
 #endif
