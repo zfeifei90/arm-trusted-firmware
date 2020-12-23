1e58668a55617b28474869190e75939c97d29fd1
Nicolas Toromanoff <nicolas.toromanoff@st.com>
2020-12-23 16:01:25 +0100

    auth: correct sign-compare warning
    Correct the warning due to compare signed and non signed variable.
    
    Signed-off-by: Nicolas Toromanoff <nicolas.toromanoff@st.com>
    Change-Id: Ic12527f5f92a34e925bee3047c168eacf5e99d8a
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/tf-a/+/189410
    Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
    Reviewed-by: Lionel DEBIEVE <lionel.debieve@st.com>
    Reviewed-by: Yann GAUTIER <yann.gautier@st.com>

--- drivers/auth/mbedtls/mbedtls_x509_parser.c
+++ drivers/auth/mbedtls/mbedtls_x509_parser.c
@@ -114,10 +114,10 @@ static int get_ext(const char *oid, void **ext, unsigned int *ext_len)
 		oid_len = mbedtls_oid_get_numeric_string(oid_str,
 							 MAX_OID_STR_LEN,
 							 &extn_oid);
-		if (oid_len == MBEDTLS_ERR_OID_BUF_TOO_SMALL) {
+		if ((oid_len == MBEDTLS_ERR_OID_BUF_TOO_SMALL) || (oid_len < 0)) {
 			return IMG_PARSER_ERR;
 		}
-		if ((oid_len == strlen(oid_str)) && !strcmp(oid, oid_str)) {
+		if (((size_t)oid_len == strlen(oid_str)) && !strcmp(oid, oid_str)) {
 			*ext = (void *)p;
 			*ext_len = (unsigned int)len;
 			return IMG_PARSER_OK;
