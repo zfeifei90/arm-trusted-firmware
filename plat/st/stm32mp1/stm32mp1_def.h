8411a39ec64b16bf4aad5211c7c38fd0efbc2e7c
Yann Gautier <yann.gautier@st.com>
2021-11-19 17:38:18 +0100

    feat(stm32mp1): add PLAT_NB_SUSPEND_MODES
    This is required for regulator suspend/resume during low-power phases.
    
    Change-Id: I6544c505fb799812ecb0a4022a09ba8b5159f13b
    Signed-off-by: Yann Gautier <yann.gautier@st.com>

--- plat/st/stm32mp1/stm32mp1_def.h
+++ plat/st/stm32mp1/stm32mp1_def.h
@@ -626,6 +626,8 @@ static inline uint32_t tamp_bkpr(uint32_t idx)
 #define PLAT_NB_RDEVS			U(19)
 /* 2 FIXED */
 #define PLAT_NB_FIXED_REGS		U(2)
+/* Number of low power modes defined in the device tree */
+#define PLAT_NB_SUSPEND_MODES		7
 
 /*******************************************************************************
  * Device Tree defines
