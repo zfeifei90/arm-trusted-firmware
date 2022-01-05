777fc39869495f7dc8d8853f753d5de68ce43016
Yann Gautier <yann.gautier@foss.st.com>
2022-01-05 18:02:46 +0100

    fix(st): manage UART clock and reset only in BL2
    As the UART is already initialized, no need to check for UART clock or
    reset in next BL.
    
    Signed-off-by: Yann Gautier <yann.gautier@foss.st.com>
    Change-Id: I044ef2386abe2d3dba5a53c3685440d64ca50a4f
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/tf-a/+/234598
    Reviewed-by: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Reviewed-by: Lionel DEBIEVE <lionel.debieve@foss.st.com>
    Reviewed-by: Nicolas LE BAYON <nicolas.le.bayon@st.com>

--- plat/st/common/stm32mp_common.c
+++ plat/st/common/stm32mp_common.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2021, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2015-2022, ARM Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -249,7 +249,7 @@ static void set_console(uintptr_t base, uint32_t clk_rate)
 
 int stm32mp_uart_console_setup(void)
 {
-	uint32_t clk_rate;
+	uint32_t clk_rate = 0U;
 	int result;
 	uint32_t boot_itf __unused;
 	uint32_t boot_instance __unused;
@@ -257,11 +257,16 @@ int stm32mp_uart_console_setup(void)
 	result = dt_get_stdout_uart_info(&dt_uart_info);
 
 	if ((result <= 0) ||
-	    (dt_uart_info.status == DT_DISABLED) ||
-	    (dt_uart_info.clock < 0) ||
+	    (dt_uart_info.status == DT_DISABLED)) {
+		return -ENODEV;
+	}
+
+#if defined(IMAGE_BL2)
+	if ((dt_uart_info.clock < 0) ||
 	    (dt_uart_info.reset < 0)) {
 		return -ENODEV;
 	}
+#endif
 
 #if STM32MP_UART_PROGRAMMER || !defined(IMAGE_BL2)
 	stm32_get_boot_interface(&boot_itf, &boot_instance);
@@ -276,15 +281,13 @@ int stm32mp_uart_console_setup(void)
 	if (dt_set_stdout_pinctrl() != 0) {
 		return -ENODEV;
 	}
-#endif
 
 	clk_enable((unsigned long)dt_uart_info.clock);
 
-#if defined(IMAGE_BL2)
 	reset_uart((uint32_t)dt_uart_info.reset);
-#endif
 
 	clk_rate = clk_get_rate((unsigned long)dt_uart_info.clock);
+#endif
 
 	set_console(dt_uart_info.base, clk_rate);
 
