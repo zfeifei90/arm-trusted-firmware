zfeifei90/arm-trusted-firmware

commit c62f7e6839c0948efecd648a69a9ae88f37985d9

stm32mp1: get pin_count from the gpio-ranges property
The "ngpios" property is deprecated and may be removed.
Use the "gpio-ranges" property where the last parameter of that property
is the number of available pins within that range.

Signed-off-by: Fabien Dessenne <fabien.dessenne@foss.st.com>
Change-Id: I28295412c7cb1246fc753cff0d447b6fdcdc4c0f

plat/st/common/stm32mp_dt.c
@@ -395,6 +395,9 @@ int fdt_get_gpio_bank_pin_count(unsigned int bank)
 {
 	int pinctrl_node;
 	int node;
+	int len;
+	int i;
+	int pin_count = 0;
 	uint32_t bank_offset;
 
 	pinctrl_node = stm32_get_gpio_bank_pinctrl_node(fdt, bank);
@@ -424,12 +427,19 @@ int fdt_get_gpio_bank_pin_count(unsigned int bank)
 			return 0;
 		}
 
-		cuint = fdt_getprop(fdt, node, "ngpios", NULL);
-		if (cuint == NULL) {
-			return -FDT_ERR_NOTFOUND;
+		/* Parse gpio-ranges with its 4 parameters */
+		cuint = fdt_getprop(fdt, node, "gpio-ranges", &len);
+		len /= sizeof(*cuint);
+		if ((len % 4) != 0) {
+			return -FDT_ERR_BADVALUE;
 		}
 
-		return (int)fdt32_to_cpu(*cuint);
+		for (i = 0; i < len / 4; i++) {
+			pin_count += fdt32_to_cpu(*(cuint + 3));
+			cuint += 4;
+		}
+
+		return pin_count;
 	}
 
 	return 0;

commit 75e75f9550acc6bd2d166358b6fc917fc63a1adb

fix(stm32mp1): consider the GPIO offset to expose all the GPIO lines
Consider the GPIO controller offset (from "gpio-ranges") to compute the
maximum GPIO line number.
This fixes an issue where gpio-ranges uses a non-null offset.
  e.g.: gpio-ranges = <&pinctrl 6 86 10>)
        in that case the last valid GPIO line is not 9 but 15 (6 + 10 - 1)

Fixes: c62f7e6839c0 ("stm32mp1: get pin_count from the gpio-ranges property")

Signed-off-by: Fabien Dessenne <fabien.dessenne@foss.st.com>
Change-Id: I5d21f012efc7a83e9615630848f33164961e153e
Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/tf-a/+/237809
Reviewed-by: CITOOLS <MDG-smet-aci-reviews@list.st.com>
Reviewed-by: Yann GAUTIER <yann.gautier@foss.st.com>
Reviewed-by: CIBUILD <MDG-smet-aci-builds@list.st.com>

plat/st/common/stm32mp_dt.c
@@ -618,7 +618,7 @@ int fdt_get_gpio_bank_pin_count(unsigned int bank)
 	int node;
 	int len;
 	int i;
-	int pin_count = 0;
+	int pin_count;
 	uint32_t bank_offset;
 
 	pinctrl_node = stm32_get_gpio_bank_pinctrl_node(fdt, bank);
@@ -655,8 +655,11 @@ int fdt_get_gpio_bank_pin_count(unsigned int bank)
 			return -FDT_ERR_BADVALUE;
 		}
 
+		/* Get the last defined gpio line (offset + nb of pins) */
+		pin_count = fdt32_to_cpu(*(cuint + 1)) + fdt32_to_cpu(*(cuint + 3));
 		for (i = 0; i < len / 4; i++) {
-			pin_count += fdt32_to_cpu(*(cuint + 3));
+			pin_count = MAX(pin_count, (int)(fdt32_to_cpu(*(cuint + 1)) +
+							 fdt32_to_cpu(*(cuint + 3))));
 			cuint += 4;
 		}
 