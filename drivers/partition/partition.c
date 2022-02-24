33493a80315564213cb18d9f09cb89bc8595c8cb
Lionel Debieve <lionel.debieve@foss.st.com>
2022-02-24 18:56:28 +0100

    feat(partition): allow to find partition by type uuid
    Add function to return the partition by type.
    
    Signed-off-by: Lionel Debieve <lionel.debieve@foss.st.com>
    Change-Id: I87729dc5e68fbc45a523c894b67595b0079dd8fb
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/tf-a/+/241315
    Reviewed-by: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Reviewed-by: CIBUILD <MDG-smet-aci-builds@list.st.com>
    Reviewed-by: Nicolas LE BAYON <nicolas.le.bayon@st.com>
    Reviewed-by: Yann GAUTIER <yann.gautier@foss.st.com>

--- drivers/partition/partition.c
+++ drivers/partition/partition.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2019, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2016-2022, ARM Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -244,6 +244,19 @@ const partition_entry_t *get_partition_entry(const char *name)
 			return &list.list[i];
 		}
 	}
+	return NULL;
+}
+
+const partition_entry_t *get_partition_entry_by_type(const uuid_t *type_uuid)
+{
+	int i;
+
+	for (i = 0; i < list.entry_count; i++) {
+		if (guidcmp(type_uuid, &list.list[i].type_guid) == 0) {
+			return &list.list[i];
+		}
+	}
+
 	return NULL;
 }
 
