72e4000f6fc5c1f65e7ed132132ddaff7ea7298c
Gabriel Fernandez <gabriel.fernandez@foss.st.com>
2022-07-08 14:42:19 +0200

    feat(st-clock): don't gate/ungate an oscillator if it is not wired
    If the oscillator is not present, the gating will fail.
    
    Change-Id: If9119460a4bcd42053537f1975afe5fe1df05752
    Signed-off-by: Gabriel Fernandez <gabriel.fernandez@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/tf-a/+/259859
    Reviewed-by: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Reviewed-by: CIBUILD <MDG-smet-aci-builds@list.st.com>
    Reviewed-by: Yann GAUTIER <yann.gautier@foss.st.com>
    Domain-Review: Yann GAUTIER <yann.gautier@foss.st.com>

--- drivers/st/clk/clk-stm32-core.c
+++ drivers/st/clk/clk-stm32-core.c
@@ -966,6 +966,10 @@ int clk_stm32_osc_gate_enable(struct stm32_clk_priv *priv, int id)
 {
 	struct clk_oscillator_data *osc_data = clk_oscillator_get_data(priv, id);
 
+	if (osc_data->frequency == 0UL) {
+		return 0;
+	}
+
 	_clk_stm32_gate_enable(priv, osc_data->gate_id);
 
 	if (_clk_stm32_gate_wait_ready(priv, osc_data->gate_rdy_id, true) != 0U) {
@@ -979,6 +983,10 @@ int clk_stm32_osc_gate_enable(struct stm32_clk_priv *priv, int id)
 void clk_stm32_osc_gate_disable(struct stm32_clk_priv *priv, int id)
 {
 	struct clk_oscillator_data *osc_data = clk_oscillator_get_data(priv, id);
+
+	if (osc_data->frequency == 0UL) {
+		return;
+	}
 
 	_clk_stm32_gate_disable(priv, osc_data->gate_id);
 
